2a0d89abeb83b1be5fb968c55145f9fba306fcf9
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2024-11-22 12:06:33 +0100

    essai

--- script/main.py
+++ script/main.py
@@ -1,6 +1,16 @@
 from math import cos, sin, pi
 import panel as pn
 
+from bokeh.plotting import figure
+
+p1 = figure(height=200, sizing_mode='stretch_width')
+p2 = figure(height=200, sizing_mode='stretch_width')
+
+p1.line([1, 2, 3], [1, 2, 3])
+p2.circle([1, 2, 3], [1, 2, 3])
+
+pn.Row(p1, p2)
+
 pn.extension(sizing_mode="stretch_width")
 
 slider = pn.widgets.FloatSlider(start=0, end=360, name='Amplitude')
