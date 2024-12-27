71f55194655af931272033e85a0e3473457e18c1
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2024-12-27 11:37:25 +0100

    Acceuil revisité

--- script/script.js
+++
@@ -1,8 +0,0 @@
-var quotes = [
-    "Hello bienvenue",
-    "Bonjour",
-    "Bonne nuit du matin"
-]
-
-var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-document.getElementById("random_quote").innerHTML = randomQuote;
