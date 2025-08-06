a36db0e83d0b17c8e93bc0326ac4fb518739485c
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2025-08-06 13:27:56 +0200

    dev link update

--- script/leaflet.js
+++ script/leaflet.js
@@ -1,66 +1,43 @@
-const map = L.map('map').setView([39.74739, -105], 13);
+const map = L.map('map').setView([47, 0], 5);
 
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-	const baseballIcon = L.icon({
-		iconUrl: 'baseball-marker.png',
-		iconSize: [32, 37],
-		iconAnchor: [16, 37],
-		popupAnchor: [0, -28]
-	});
+function onEachFeature(feature, layer) {
+	let popupContent = `<p>Ligne n°${feature.properties.code_ligne}<br>${feature.properties.lib_ligne}<br>Statut: ${feature.properties.statut}</p>`;
 
-	function onEachFeature(feature, layer) {
-		let popupContent = `<p>I started out as a GeoJSON ${feature.geometry.type}, but now I'm a Leaflet vector!</p>`;
-
-		if (feature.properties && feature.properties.popupContent) {
-			popupContent += feature.properties.popupContent;
-		}
-
-		layer.bindPopup(popupContent);
+	if (feature.properties && feature.properties.popupContent) {
+		popupContent += feature.properties.popupContent;
 	}
 
-	/* global campus, bicycleRental, freeBus, coorsField */
-	const bicycleRentalLayer = L.geoJSON([bicycleRental, campus], {
+	layer.bindPopup(popupContent);
+}
 
-		style(feature) {
-			return feature.properties && feature.properties.style;
-		},
-
-		onEachFeature,
-
-		pointToLayer(feature, latlng) {
-			return L.circleMarker(latlng, {
-				radius: 8,
-				fillColor: '#ff7800',
-				color: '#000',
-				weight: 1,
-				opacity: 1,
-				fillOpacity: 0.8
-			});
+const trainLinesLayer = L.geoJSON(trainLines, {
+	style: function(feature) {
+		switch (feature.properties.statut) {
+			case 'Exploitée': return {color: "#8a2be2"};
+			case 'Fermée': return {color: "#dc143c"};
+			case 'Neutralisée': return {color: "#8b0000"};
+			case 'Transférée en voie de service': return {color: "#800080"};
+			case 'Déclassée vendue': return {color: "#e9967a"};
+			case 'Déclassée non vendue': return {color: "#fa8072"};
+			case 'Fermée avec maintien en place de la voie': return {color: "#4169e1"};
+			case 'Fermée et déposée (Plus utilisable)': return {color: "#008000"};
+			case 'Fermée non déposée (Plus utilisable)': return {color: "#32cd32"};
+			case 'Fermée et mise à disposition de tiers': return {color: "#90ee90"};
+			case 'Neutralisée et conservée pour les besoins de la Défense': return {color: "#d2691e"};
+			case 'Retranchée (Plus utilisable)': return {color: "#696969"};
 		}
-	}).addTo(map);
-
-	const freeBusLayer = L.geoJSON(freeBus, {
-
-		filter(feature, layer) {
-			if (feature.properties) {
-				// If the property "underConstruction" exists and is true, return false (don't render features under construction)
-				return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
-			}
-			return false;
-		},
-
-		onEachFeature
-	}).addTo(map);
-
-	const coorsLayer = L.geoJSON(coorsField, {
-
-		pointToLayer(feature, latlng) {
-			return L.marker(latlng, {icon: baseballIcon});
-		},
-
-		onEachFeature
-	}).addTo(map);
+	},
+	filter(feature, layer) {
+		if (feature.properties) {
+			// If the property "underConstruction" exists and is true, return false (don't render features under construction)
+			return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+		}
+		return false;
+	},
+	onEachFeature
+}).addTo(map);
