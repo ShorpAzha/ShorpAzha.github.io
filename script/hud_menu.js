d5e31dec7fb42c8e82f7173f67ee91ed697ce136
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2025-08-07 13:37:21 +0200

    m

--- script/hud_menu.js
+++ script/hud_menu.js
@@ -1,9 +1,9 @@
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
-    document.getElementById("padElements").style.paddingLeft = "255px";
+    document.getElementById("padElements").style.marginLeft = "255px";
 }
 
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
-    document.getElementById("padElements").style.paddingLeft = "0px";
+    document.getElementById("padElements").style.marginLeft = "0px";
 }
