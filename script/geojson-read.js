dd66c7a51c70f4a48d6a2107b6c644195bf4bc8f
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2025-08-07 16:52:49 +0200

    test

--- script/geojson-read.js
+++ script/geojson-read.js
@@ -1,47 +1,49 @@
-// Chargement des données GeoJSON à partir d'un fichier externe
-fetch('https://ShorpAzha.github.io/script/geojson/l_710.geojson')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erreur lors du chargement du fichier GeoJSON');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Création de la carte Leaflet
-        const map = L.map('map').setView([45.6062, 2.7449], 13);
+function geoJsonMiniMap(geoJsonFile) {
+    // Chargement des données GeoJSON à partir d'un fichier externe
+    fetch(geoJsonFile)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors du chargement du fichier GeoJSON');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Création de la carte Leaflet
+            const map = L.map('map').setView([45.6062, 2.7449], 13);
 
-        // Ajout d'une couche de tuiles
-        const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-	        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+            // Ajout d'une couche de tuiles
+            const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
 
-        // Ajout de Pop-Up 
-        function onEachFeature(feature, layer) {
-            let popupContent = `<p>${feature.properties.name}<br>${feature.properties.description}</p>`;
+            // Ajout de Pop-Up 
+            function onEachFeature(feature, layer) {
+                let popupContent = `<p>${feature.properties.name}<br>${feature.properties.description}</p>`;
 
-            if (feature.properties && feature.properties.popupContent) {
-                popupContent += feature.properties.popupContent;
+                if (feature.properties && feature.properties.popupContent) {
+                    popupContent += feature.properties.popupContent;
+                }
+
+                layer.bindPopup(popupContent);
             }
 
-            layer.bindPopup(popupContent);
-        }
-
-        // Ajout des données GeoJSON à la carte
-        const dataLayer = L.geoJSON(data, {
-            style: function(feature) {
-                switch (feature.properties.compagnie) {
-                    case 'PO': return {color: "#999"};
-                }
-            },
-            filter(feature, layer) {
-                if (feature.properties) {
-                    // If the property "underConstruction" exists and is true, return false (don't render features under construction)
-                    return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
-                }
-                return false;
-            },
-            onEachFeature
-        }).addTo(map);
-    })
-    .catch(error => console.error('Erreur:', error));
+            // Ajout des données GeoJSON à la carte
+            const dataLayer = L.geoJSON(data, {
+                style: function(feature) {
+                    switch (feature.properties.compagnie) {
+                        case 'PO': return {color: "#999"};
+                    }
+                },
+                filter(feature, layer) {
+                    if (feature.properties) {
+                        // If the property "underConstruction" exists and is true, return false (don't render features under construction)
+                        return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+                    }
+                    return false;
+                },
+                onEachFeature
+            }).addTo(map);
+        })
+        .catch(error => console.error('Erreur:', error));
+    }
