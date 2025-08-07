2f5061418f30d0e994be7df2e53d70cc18ddcc3a
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2025-08-07 14:13:47 +0200

    a

--- script/leaflet.js
+++ script/leaflet.js
@@ -1,3 +1,9 @@
+function fetchJSON(url) {
+	return fetch(url).then(function(response) {
+		return response.json();
+	});
+}
+
 var trainLines = fetchJSON('script/geojson/l_600.geojson')
 
 const map = L.map('map').setView([47, 0], 5);
