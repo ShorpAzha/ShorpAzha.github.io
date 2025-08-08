aaa2176837a9bb7316d74f68476001fda0abd22c
Shorp <151829694+ShorpAzha@users.noreply.github.com>
2025-08-08 21:40:01 +0200

    update data

--- script/json-data-read.js
+++ script/json-data-read.js
@@ -1,5 +1,5 @@
 const data_url = 'https://ShorpAzha.github.io/script/json/data.json';
-const data_max = 6;
+const data_max = 7;
 
 function updateCompteurInURL(nouvelleValeur) {
     const url = new URL(window.location);
